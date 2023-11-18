2c714706bc63db29d71e87e8dd3ab27581c4017f
roxsand <132316834+roxsand@users.noreply.github.com>
2023-11-18 21:00:48 +0800

    Aboutus.js/css,  index.js background2 and Dog

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './Components/Home';
+import Aboutus from './Components/Aboutus';
 import UserProfile from './Components/UserProfile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <UserProfile /> */}
+    <Aboutus />
     {/* <Home /> */}
   </React.StrictMode>
 );
