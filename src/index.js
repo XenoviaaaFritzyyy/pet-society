464079b1ce99d7171fe96406c2beb4e7db2fbec6
Borii1 <borinaga911@gmail.com>
2023-11-18 18:54:48 +0800

    Layout things

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './Components/Home';
+// import Home from './Components/Home';
 import UserProfile from './Components/UserProfile';
 import PetProfile from './Components/PetProfile';
 import ApplicationForm from './Components/ApplicationForm';
