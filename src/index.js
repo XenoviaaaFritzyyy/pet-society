5060f22c7e7c42670f9034cb0721b4e2d7eb50a7
Hazelyn123 <hazelynbalingcasag123@gmail.com>
2023-11-18 21:21:35 +0800

    Admin added and minor changes in some file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import UserProfile from './Components/UserProfile';
 import PetProfile from './Components/PetProfile';
 import ApplicationForm from './Components/ApplicationForm';
+import Admin from './Components/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
