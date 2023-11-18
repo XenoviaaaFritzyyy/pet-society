db9f96d7afb9e5cd3bff84fcae65f5375036de98
XenoviaaaFritzyyy <132067627+XenoviaaaFritzyyy@users.noreply.github.com>
2023-11-18 23:34:43 +0800

    Gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,16 +1,140 @@
-<<<<<<< HEAD
-=======
-import React from "react";
+import React, { useState } from "react";
 import Navbar from './Navbar';
-function Gallery(){
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-    return (
-        <>
-            <Navbar />
-            
-        </>
-    );
+function Gallery() {
+  const [expanded, setExpanded] = useState(false);
+  const [isFavoriteClicked, setIsFavoriteClicked] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleFavoriteClick = () => {
+    setIsFavoriteClicked(!isFavoriteClicked);
+  };
+
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  return (
+    <>
+      <Navbar />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        {/* Container with flexbox styling */}
+        <Card sx={{ maxWidth: 700 }}>
+          <CardMedia
+            component="img"
+            height="450"
+            image="/images/rob.jpg"
+          />
+          <CardContent>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant="h5" color="text.primary">
+                Shrimp and Chorizo Paella
+              </Typography>
+              <IconButton
+                aria-label="add to favorites"
+                onClick={handleFavoriteClick}
+                style={{ color: isFavoriteClicked ? 'red' : 'inherit' }}
+              >
+                <FavoriteIcon />
+              </IconButton>
+            </div>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            {/* Additional actions if needed */}
+          </CardActions>
+        </Card>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        {/* Container with flexbox styling */}
+        <Card sx={{ maxWidth: 700 }}>
+          <CardMedia
+            component="img"
+            height="450"
+            image="/images/rob.jpg"
+          />
+          <CardContent>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant="h5" color="text.primary">
+                Shrimp and Chorizo Paella
+              </Typography>
+              <IconButton
+                aria-label="add to favorites"
+                onClick={handleFavoriteClick}
+                style={{ color: isFavoriteClicked ? 'red' : 'inherit' }}
+              >
+                <FavoriteIcon />
+              </IconButton>
+            </div>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            {/* Additional actions if needed */}
+          </CardActions>
+        </Card>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        {/* Container with flexbox styling */}
+        <Card sx={{ maxWidth: 700 }}>
+          <CardMedia
+            component="img"
+            height="450"
+            image="/images/rob.jpg"
+          />
+          <CardContent>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant="h5" color="text.primary">
+                Shrimp and Chorizo Paella
+              </Typography>
+              <IconButton
+                aria-label="add to favorites"
+                onClick={handleFavoriteClick}
+                style={{ color: isFavoriteClicked ? 'red' : 'inherit' }}
+              >
+                <FavoriteIcon />
+              </IconButton>
+            </div>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            {/* Additional actions if needed */}
+          </CardActions>
+        </Card>
+      </div>
+    </>
+  );
 }
 
-export default Gallery
->>>>>>> 427390ba9dad2799fa59d8af1d843cf9406018d7
+export default Gallery;
