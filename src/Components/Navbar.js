6c2f75ae0270834e456adee8c198a0d9841fa0a2
Borii1 <borinaga911@gmail.com>
2023-11-18 12:32:23 +0800

    New profile picture and ang dropdown kay ma navigate na siya

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 import '../Css/Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const navigate = useNavigate(); // Hook for programmatic navigation
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -19,19 +20,36 @@ function Navbar() {
     const changeOption = (option) => {
       setSelectedOption(option);
       toggleDropdown(); // Close the dropdown after an option is selected
-      alert(`Selected option: ${option}`);
+      handleOptionClick(option); // Navigate based on the selected option
+    };
+
+    const handleOptionClick = (option) => {
+      switch (option) {
+        case 'Profile':
+          navigate('/profile'); // Adjust the path as needed
+          break;
+        case 'Request':
+          navigate('/request'); // Adjust the path as needed
+          break;
+        case 'Log out':
+          navigate('/'); // Navigate to the home page or wherever needed
+          break;
+        // Add more cases as needed
+        default:
+          break;
+      }
     };
   
     return (
       <div className="profile-dropdown">
         <div className="selected-option" onClick={toggleDropdown}>
-            <img src="/images/background1.png" alt="User Profile" className="user-profile-image" />
+            <img src="/images/rob.jpg" alt="User Profile" className="user-profile-image" />
         </div>
         {isDropdownOpen && (
           <div className="profile-dropdown-content">
-            <div onClick={() => changeOption('Settings')}>Settings</div>
-            <div onClick={() => changeOption('Account')}>Account</div>
-            <div onClick={() => changeOption('Other Option')}>Other Option</div>
+            <div onClick={() => changeOption('Profile')}>Profile</div>
+            <div onClick={() => changeOption('Request')}>Request</div>
+            <div onClick={() => changeOption('Log out')}>Log out</div>
             {/* Add more options as needed */}
           </div>
         )}
