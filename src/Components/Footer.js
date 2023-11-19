49ea9418ba921f4ebcaae476c84e7b57d95aa80b
XenoviaaaFritzyyy <132067627+XenoviaaaFritzyyy@users.noreply.github.com>
2023-11-19 20:04:34 +0800

    Slight adjustment sa Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,3 @@
-// Footer.js
 import React from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import '../Css/Footer.css';
@@ -15,29 +14,29 @@ const Footer = () => {
       }}
     >
       <Container maxWidth="lg">
-        <Grid container direction="row" alignItems="center" justifyContent="center">
-          <Grid item xs={12}>
-            <Typography className="footer-contact" variant="h5">
-              Contact Us
-            </Typography>
-          </Grid>
-          <Grid item xs={12} className="contact-info">
-            <Typography variant="subtitle1">
-              Address: 143, Main Road, Skena.
-            </Typography>
-            <Typography variant="subtitle1">
-              Email: petsociety@gmail.com
-            </Typography>
-            <Typography variant="subtitle1">
-              Phone Number: +639191610325
-            </Typography>
-          </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="center">
+          <Grid item xs={12}
+          sx={{ display: 'flex', alignItems: 'center', gap: '68px', marginTop: '8px' }}>
+        <Typography className="footer-contact" variant="h5">
+          Contact Us
+        </Typography>
+        <Typography variant="subtitle1">
+          Address: 143, Main Road, Skena.
+        </Typography>
+        <Typography variant="subtitle1">
+          Email: petsociety@gmail.com
+        </Typography>
+        <Typography variant="subtitle1">
+          Phone Number: +639191610325
+        </Typography>
         </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1">
-              {`Copyright © ${new Date().getFullYear()} Pet Society`}
-            </Typography>
-          </Grid>
+      <Grid item xs={12}>
+    <Typography variant="subtitle1">
+      {`Copyright © ${new Date().getFullYear()} Pet Society`}
+    </Typography>
+  </Grid>
+</Grid>
+
       </Container>
     </Box>
   );
