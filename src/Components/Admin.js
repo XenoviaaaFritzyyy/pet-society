416ee10d775e2f2cc96c1755f17aba350e61c6c3
Hazelyn123 <hazelynbalingcasag123@gmail.com>
2023-11-20 11:58:01 +0800

    admin minor changes (link for logout)

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import '../Css/Admin.css';
 
@@ -27,7 +27,11 @@ function Admin() {
 
   return (
     <>
-      <a href="#">Logout</a>
+      <Link to="/signin" style={{ textDecoration: 'none' }}>
+        <button style={{ position: 'absolute', top: '25px', right: '50px', textDecoration: 'none', fontSize: '15px', padding: '8px 15px', borderRadius: '10px'}}>
+          Logout
+        </button>
+      </Link>
 
       <div className="admin-container">
         {showDashboard ? (
