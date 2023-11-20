48400064e2a617f4aadaf12106cd395ba3582518
roxsand <132316834+roxsand@users.noreply.github.com>
2023-11-21 00:03:26 +0800

    Aboutus, Footer and Icons. Unta ferpek na ni.

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,65 +1,84 @@
-//Aboutus.js
+// Aboutus.js
 import React from 'react';
 import Navbar from './Navbar';
 import '../Css/Aboutus.css';
 import Footer from './Footer';
 
-function Aboutus () {
+function Aboutus() {
   return (
     <>
-    <div>
-      <Navbar />
-      <div className="about-us-container">
-        <h1 className="about-us-title">Know More About Us</h1>
-        <img src="/images/Dog.jpg" alt="About Us" className="about-us-image" />
-        <p>
-          Welcome to Pet Society, where we're dedicated to making pet adoption simple and joyful.
-          Find your fur-ever friend easily in one place, without the hassle of searching through
-          multiple rescues. Pet Society, your community's trusted source, offers the easiest and
-          most secure way to adopt dogs right in your neighborhood. Join us in spreading happiness
-          to both tails and hearts.
-        </p>
+      <div>
+        <Navbar />
+        <div className="about-us-container">
+          <h1 className="about-us-title">Know More About Us</h1>
+          <div className="white-box"> {/* New div for the white box background */}
+            <img src="/images/Dog.jpg" alt="About Us" className="about-us-image" />
+          </div>
+          <div className="welcome-text">
+            <p>
+              Welcome to Pet Society, where we're dedicated to making pet adoption simple and joyful.
+              Find your fur-ever friend easily in one place, without the hassle of searching through
+              multiple rescues. Pet Society, your community's trusted source, offers the easiest and
+              most secure way to adopt dogs right in your neighborhood. Join us in spreading happiness
+              to both tails and hearts.
+            </p>
+          </div>
+          <div className="our-goal-section">
+            <h2>Our Goal</h2>
+            <p>
+              Our primary goal is to become the go-to destination for individuals seeking to adopt
+              dogs. We aim to increase successful dog adoptions, reduce the time dogs spend in
+              shelters, and create a vibrant community of dog adopters.
+            </p>
+          </div>
 
-        <div className="about-us-section">
-          <h2>Our Goal</h2>
-          <p>
-            Our primary goal is to become the go-to destination for individuals seeking to adopt
-            dogs. We aim to increase successful dog adoptions, reduce the time dogs spend in
-            shelters, and create a vibrant community of dog adopters.
-          </p>
-        </div>
-
-        <div className="about-us-section">
-          <h2>The Team</h2>
-          <div className="team-members">
-            {/* Add your team members' profile pictures */}
-            <div className="team-member">
-              <img src="/images/Fritz.jpg" alt="Team Member 1" />
-              <p>Team Member 1's Motto</p>
-            </div>
-            <div className="team-member">
-              <img src="/images/Rox.jpg" alt="Team Member 2" />
-              <p>Team Member 2's Motto</p>
-            </div>
-            <div className="team-member">
-              <img src="/images/Rob2.jpg" alt="Team Member 3" />
-              <p>Team Member 3's Motto</p>
-            </div>
-            <div className="team-member">
-              <img src="/images/Hazelyn.jpg" alt="Team Member 4" />
-              <p>Team Member 4's Motto</p>
+          <div className="Team-profile">
+            <h2>The Team</h2>
+            <div className="team-members">
+              {/* Add your team members' profile cards */}
+              <div className="team-member">
+                <div className="card">
+                  <img src="/images/Fritz.jpg" alt="Team Member 1" className="card-img-top" />
+                  <div className="card-body">
+                    <p className="card-text">I'm not a mathematician, but I know my life is a series of unfortunate fractions.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="team-member">
+                <div className="card">
+                  <img src="/images/Rox.jpg" alt="Team Member 2" className="card-img-top" />
+                  <div className="card-body">
+                    <p className="card-text">Live, Laugh, and Love</p>
+                  </div>
+                </div>
+              </div>
+              <div className="team-member">
+                <div className="card">
+                  <img src="/images/Rob2.jpg" alt="Team Member 3" className="card-img-top" />
+                  <div className="card-body">
+                    <p className="card-text">Chase adventures, not perfection</p>
+                  </div>
+                </div>
+              </div>
+              <div className="team-member">
+                <div className="card">
+                  <img src="/images/Hazelyn.jpg" alt="Team Member 4" className="card-img-top" />
+                  <div className="card-body">
+                    <p className="card-text">Smart by design, lazy by choice, and money is my favorite noise.</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Footer */}
-      <footer>
-         <Footer />
-      </footer>
-        </div>
+        {/* Footer */}
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </>
   );
-};
+}
 
 export default Aboutus;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,3 +1,5 @@
+// Footer.js
+
 import React from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import '../Css/Footer.css';
@@ -15,28 +17,42 @@ const Footer = () => {
     >
       <Container maxWidth="lg">
         <Grid container direction="column" alignItems="center" justifyContent="center">
-          <Grid item xs={12}
-          sx={{ display: 'flex', alignItems: 'center', gap: '68px', marginTop: '8px' }}>
-        <Typography className="footer-contact" variant="h5">
-          Contact Us
-        </Typography>
-        <Typography variant="subtitle1">
-          Address: 143, Main Road, Skena.
-        </Typography>
-        <Typography variant="subtitle1">
-          Email: petsociety@gmail.com
-        </Typography>
-        <Typography variant="subtitle1">
-          Phone Number: +639191610325
-        </Typography>
+          <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', gap: '68px', marginTop: '8px' }}>
+            <Typography className="footer-contact" variant="subtitle1">
+              Contact Us
+            </Typography>
+            <Typography variant="subtitle1">
+              Address: 143, Main Road, Skena
+            </Typography>
+            <Typography variant="subtitle1">
+              Email: <a href="mailto:petsociety@gmail.com" className="email-link">petsociety@gmail.com</a>
+            </Typography>
+            <Typography variant="subtitle2">
+              Phone Number: +639191610325
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+          
+            <div className="social-icons">
+              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <img src="/images/facebook-icon.png" alt="Facebook" className="social-icon" />
+              </a>
+              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <img src="/images/instagram-icon.png" alt="Instagram" className="social-icon" />
+              </a>
+              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                <img src="/images/twitter-icon.png" alt="Twitter" className="social-icon" />
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="copyright" style={{ marginTop: '8px' }}>
+            <Typography variant="subtitle1">
+              {`Copyright © ${new Date().getFullYear()} Pet Society`}
+            </Typography>
+            </div>
+          </Grid>
         </Grid>
-      <Grid item xs={12}>
-    <Typography variant="subtitle1">
-      {`Copyright © ${new Date().getFullYear()} Pet Society`}
-    </Typography>
-  </Grid>
-</Grid>
-
       </Container>
     </Box>
   );
