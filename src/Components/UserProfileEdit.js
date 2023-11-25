9162f03c51d1eeb9e5b77291eda2402c75a34aaa
roxsand <132316834+roxsand@users.noreply.github.com>
2023-11-25 21:24:01 +0800

    UserProfileEdit po ito mga ferson

--- src/Components/UserProfileEdit.js
+++ src/Components/UserProfileEdit.js
@@ -32,58 +32,65 @@ const UserProfileEdit = () => {
   
   return (
     <div className="box-container">
-  <form action="#" id="pet-profile-form" encType="multipart/form-data">
-  <div className="admin-form pet-profile">
-    <div className="details-pet">
-      <span className="title">My Profile</span>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center', // Center items horizontally
-          borderRadius: '38px',
-          backgroundColor: '#ffffff',
-          borderColor: '#cccccc',
-          padding: '10px',
-          paddingTop: '20px',
-          marginTop: '20px',
-        }}
-      >
-        <img
-          src="/images/RobFinal.jpg"
-          alt="User Profile"
-          className="user-profile-image"
-          style={{ width: '90px', height: '90px' }}
-        />
-        <Button
-          component="label"
-          color="primary"
-          variant="contained"
-          startIcon={<AddPhotoIcon />}
-          sx={{
-            backgroundColor: 'white',
-            color: '#27374D',
-            '&:hover': {
-              backgroundColor: '#142132',
-              color: 'white',
-            },
-            borderRadius: '8px',
-            border: '.1px solid #27374D',
-            display: 'flex',
-            width: 150,
-            whiteSpace: 'nowrap',
-            fontSize: 10,
-            marginTop: '10px',
-          }}
-        >
-          Upload New
-          <VisuallyHiddenInput type="file" />
-        </Button>
-      </Box>
+      <form action="#" id="pet-profile-form" encType="multipart/form-data">
+        <div className="admin-form pet-profile">
+          <div className="details-pet">
+            <span className="title">My Profile</span>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row', // Align items horizontally
+                alignItems: 'center', // Center items vertically
+                borderRadius: '38px',
+                backgroundColor: '#ffffff',
+                borderColor: '#cccccc',
+                padding: '10px',
+                paddingTop: '20px',
+                marginTop: '20px',
+              }}
+            >
+              <img
+                src="/images/RobFinal.jpg"
+                alt="User Profile"
+                className="user-profile-image"
+                style={{
+                  width: '90px',
+                  height: '90px',
+                  marginRight: '20px',
+                  objectFit: 'cover', // or 'contain' based on your preference
+                  borderRadius: '50%', // Optional: add this if you want a circular image
+                }}
+              />
+              <div>
+                <Button
+                  component="label"
+                  color="primary"
+                  variant="contained"
+                  startIcon={<AddPhotoIcon />}
+                  sx={{
+                    backgroundColor: 'white',
+                    color: '#27374D',
+                    '&:hover': {
+                      backgroundColor: '#142132',
+                      color: 'white',
+                    },
+                    borderRadius: '8px',
+                    border: '.1px solid #27374D',
+                    display: 'flex',
+                    whiteSpace: 'nowrap',
+                    fontSize: 10,
+                    marginTop: '10px',
+                  }}
+                >
+                  Upload New
+                  <VisuallyHiddenInput type="file" />
+                </Button>
+              </div>
+            </Box>
+          </div>
+        </div>
+      </form>
     </div>
-  </div>
-</form>
-  </div>
   );
 };
 
